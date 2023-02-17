979b3e1e90bc34d43c27750b02f9c45fca28f7a1
A-nita18 <anitasofia254@gmail.com>
2023-02-17 08:52:33 +0300

    tables in python

--- Week-2/numbers.py
+++ Week-2/numbers.py
@@ -1,9 +1,11 @@
+#Assignment 10 : Get the average of numbers in a list by first entering them as input
 numbers = []
 
-sum_num = 0
-numbers.append(input("Enter the first ten numbers: "))
-for number in numbers:
-    sum_num = sum_num + int(number)
+n = int(input("Enter the number of elements: "))
+for i in range(0,n):
+    element = int(input("Enter the elements: "))
+    numbers.append(element)
 
-avg_num = sum_num / 10
-print(avg_num)
+avg_num = sum(numbers)/n
+print("The created list: ",numbers)
+print("The average is: ",avg_num)
